15d5b4b9458224eebea57c73401b03dfa4f5e48d
joshmarto <65306069+joshmarto@users.noreply.github.com>
2021-09-13 14:58:43 -0600

    Chapter 5 - Loops

---
+++ nestedLoops.py
@@ -0,0 +1,13 @@
+# NESTED LOOPS
+for row in range(5):
+    if row%2 == 0:
+        for column in range(1, 6):
+            if column%2 == 1:
+                if column != 5:
+                    print(" ", end="")
+                else:
+                    print(" ")
+            else:
+                print("|", end="")
+    else:
+        print("-----")
